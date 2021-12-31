fd1fc0cd7215c80be4a3c41e21a1499c37654408
hectorzaragoza <zaragoza09@gmail.com>
2021-12-30 22:32:26 -0800

    Added a ListView class for a view because I am dealing with a view that is specifically trying to handle a list of items from my db to pass to my template

--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from .models import Review
+
 # from .models import Review
 
 # Create your views here.
@@ -43,14 +45,21 @@ class ThankYouView(TemplateView):
         context["message"] = "This Works!"
         return context
 
-class ReviewsListView(TemplateView):
-    template_name = "reviews/review_list.html"
+# class ReviewsListView(TemplateView):
+#     template_name = "reviews/review_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         reviews = Review.objects.all()
+#         context["reviews"] = reviews
+#         return context
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    # override object_list to set your context as reviews
+    context_object_name = "reviews"
+    
 
 class SingleReviewView(TemplateView):
     template_name = "reviews/single_review.html"
